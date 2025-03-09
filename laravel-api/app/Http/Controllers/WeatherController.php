8007056f846e85b99a683bc1eb61a20a4cb0d477
Macgayver Rosa <macgayver.rosa@outlook.com>
2025-03-09 21:53:24 +1100

    adding welcome email

--- laravel-api/app/Http/Controllers/WeatherController.php
+++ laravel-api/app/Http/Controllers/WeatherController.php
@@ -10,10 +10,9 @@ class WeatherController extends Controller
 {
     public function getWeather()
     {
-        // Cache the response for 15 minutes to minimize API calls
         $weatherData = Cache::remember('weather_data', 15 * 60, function () {
-            $apiKey = env('WEATHER_API_KEY');  // Store API key in .env
-            $city = 'Perth'; // Change city if needed
+            $apiKey = env('WEATHER_API_KEY');
+            $city = 'Perth';
             $url = "https://api.weatherapi.com/v1/current.json?key={$apiKey}&q={$city}";
 
             $response = Http::get($url);
